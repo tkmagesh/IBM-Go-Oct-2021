dadbca46bb8a1effa8407a54184f3a05f41917e5
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-05 14:17:30 +0530

    assignment - 2

---
+++ 02-programming-constructs/assignment-2.go
@@ -0,0 +1,10 @@
+/*
+	Display the following menu until the user choses to exit
+		1. Add
+		2. Subtract
+		3. Multiply
+		4. Divide
+		5. Exit
+
+	If the user enters any other choice, display "Invalid choice" and display the menu again.
+*/

6471b606295e4055eebbdf86f776571382f3dded
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-05 17:01:01 +0530

    assignment solution

--- 03-functions/assignment-5.go
+++ 03-functions/assignment-5.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	increment := getIncrement()
+	increment, decrement := getCounter()
 	fmt.Println(increment()) //=> 1
 	fmt.Println(increment()) //=> 2
 	fmt.Println(increment()) //=> 3
@@ -11,16 +11,22 @@ func main() {
 
 	//counter = 10000 //influencing the outcome of the increment function
 
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
-	fmt.Println(increment())
+	fmt.Println(decrement()) // => 3
+	fmt.Println(decrement()) // => 2
+	fmt.Println(decrement()) // => 1
+	fmt.Println(decrement()) // => 0
 }
 
-func getIncrement() func() int {
+func getCounter() (func() int, func() int) {
+
 	var counter int
-	return func() int {
+	increment := func() int {
 		counter++
 		return counter
 	}
+	decrement := func() int {
+		counter--
+		return counter
+	}
+	return increment, decrement
 }

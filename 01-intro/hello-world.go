dfbb9b169219d799e3e7a6747e78c3e0f369f919
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-05 11:50:38 +0530

    data types in go

---
+++ 01-intro/hello-world.go
@@ -0,0 +1,17 @@
+/* package declaration */
+package main
+
+/* import other packages */
+import "fmt"
+
+/* package level types */
+
+/* package init function */
+
+/* main function */
+func main() {
+	/* main function code here */
+	fmt.Println("Hello World")
+}
+
+/* other functions */

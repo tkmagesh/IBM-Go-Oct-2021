49e9298f6239b4c72cbf304e1d713a07a2f717a9
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-05 17:26:57 +0530

    deferred functions

---
+++ 03-functions/demo-08.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		fmt.Println("[@main] Deferred")
+	}()
+	fmt.Println(f1())
+	fmt.Println("main invoked")
+}
+
+func f1() (result int) {
+	defer func() {
+		fmt.Println("[@f1] Deferred - 1")
+		result = 500
+	}()
+	defer func() {
+		fmt.Println("[@f1] Deferred - 2")
+	}()
+	defer func() {
+		fmt.Println("[@f1] Deferred - 3")
+		result = 200
+	}()
+	fmt.Println("f1 invoked")
+	f2()
+	result = 100
+	return
+}
+
+func f2() {
+	defer func() {
+		fmt.Println("[@f2] Deferred - 1")
+	}()
+	defer func() {
+		fmt.Println("[@f2] Deferred - 2")
+	}()
+	defer func() {
+		fmt.Println("[@f2] Deferred - 3")
+	}()
+	fmt.Println("f2 invoked")
+}

c8978e2cc182ff562df8449772e15f516155243e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-06 15:06:02 +0530

    using thrid party packages

--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"modularity-demo/calculator"
 	"modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
+
+	color.Red(fmt.Sprintf("%t\n", utils.IsEven(200)))
+	color.Blue(fmt.Sprintf("%d\n", calculator.Add(100, 200)))
+	color.Green(fmt.Sprintf("%d\n", calculator.Subtract(100, 200)))
+	color.Yellow(fmt.Sprintf("%d\n", calculator.GetOpCount()))
+
 	fmt.Println(utils.IsEven(200))
 	fmt.Println(calculator.Add(100, 200))
-	fmt.Println(calculator.Subtract(100, 200))
-	fmt.Println(calculator.GetOpCount())
 
 }

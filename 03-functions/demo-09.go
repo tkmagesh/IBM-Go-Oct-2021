473212128abd085808e53715635668ed328eb2d3
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-06 09:56:29 +0530

    panic & recovery

---
+++ 03-functions/demo-09.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("[@main] Error:", err)
+		}
+	}()
+	result, err := divide(100, 0)
+	if err != nil {
+		fmt.Println("Something went wrong : ", err)
+		return
+	}
+	fmt.Println(result)
+}
+
+func divide(x, y int) (result int, err error) {
+	defer func() {
+		if er := recover(); er != nil {
+			fmt.Println("[@divide] Error:", er)
+			err = errors.New("Cannot divide by zero!")
+		}
+	}()
+	if y == 0 {
+		panic("Cannot divide by zero!")
+	}
+	result, err = x/y, nil
+	return
+}

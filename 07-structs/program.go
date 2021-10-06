d0b58d46344b86091b133c601b09c72ac28b0b5b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-06 15:54:11 +0530

    updates

--- 07-structs/program.go
+++ 07-structs/program.go
@@ -23,6 +23,14 @@ func main() {
 	/*
 		var product Product = Product{101, "Pen", 5, 100, "Stationery"}
 	*/
-	var product Product = Product{Id: 100, Cost: 5, Units: 10, Name: "Pen"}
+	/* var product Product = Product{Id: 100, Cost: 5, Units: 10, Name: "Pen"} */
+	product := Product{Id: 100, Cost: 5, Units: 10, Name: "Pen"}
 	fmt.Printf("%#v\n", product)
+	fmt.Println("After applying 10% discount")
+	applyDiscount( /*  */ )
+	fmt.Printf("%#v\n", product)
+}
+
+func applyDiscount( /* product, discount percentage */ ) {
+
 }
